be401b148fa2e87adf91b06da33ee7e3396a93de
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-27 16:58:03 +0100

    Milestone 2 - aggiustamento

--- js/main.js
+++ js/main.js
@@ -10,10 +10,18 @@ const container = document.querySelector('.img_container');
 for (let i = 0; i < imgCollection.length; i++){
 
     imgContent +='<div class=img_box><img src="img/01.webp" alt=""></div>';
+    break;
 
 }
 
 container.innerHTML += imgContent;
 document.querySelector('.img_box').classList.add('img_show');
+
+
+// Gestione frecce per successione immagini
+const upArrow = getElementById('up_arrow');
+const downArrow = getElementById('down_arrow');
+
+
 
 
